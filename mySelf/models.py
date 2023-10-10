1dc76e1b8a05f22d659b50b4d94452909cd18da9
Getonto3 <getontomoses18@gmail.com>
2023-10-10 11:46:18 +0300

    New template to display data from the database

--- mySelf/models.py
+++ mySelf/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class MySelf(models.Model):
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    nickName=models.CharField(max_length=255,null=True)
+    admNo=models.IntegerField(null=True)
--- mySelf/views.py
+++ mySelf/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader 
+from .models import MySelf
 
+# Create your views here.
 def mySelf(request):
     return HttpResponse("You know what they say, you snoze you lose!And it looks like you snost and you lost.")
 
@@ -9,4 +11,10 @@ def mySelf(request):
     template =loader.get_template('first.html')
     return HttpResponse(template.render())
 
-# Create your views here.
+def mySelf(request):
+    members= MySelf.objects.all().values()
+    template=loader.get_template('members.html')
+    context ={
+        'members':members,   
+    }
+    return HttpResponse(template.render(context, request))
