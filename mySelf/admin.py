aa8478b5116ceed93edfa965e32fee0b49aeb99d
Getonto3 <getontomoses18@gmail.com>
2023-10-23 16:13:49 +0300

    the admin page

--- mySelf/admin.py
+++ mySelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MySelf
 
 # Register your models here.
+
+class MySelfAdmin(admin.ModelAdmin):
+  list_display = ("firstName", "lastName", "nickName","admNo")
+
+admin.site.register(MySelf, MySelfAdmin)
