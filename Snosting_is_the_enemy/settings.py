1a26cfe21065aa9641780785a3a3f0532685db79
Getonto3 <getontomoses18@gmail.com>
2023-10-31 23:30:11 +0300

    Django tutorial wrap up

--- Snosting_is_the_enemy/settings.py
+++ Snosting_is_the_enemy/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Snosting_is_the_enemy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'masteruser',
+        'PASSWORD': 'intheDjango',
+        'HOST': 'getonto.cxayaxbrua5e.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
